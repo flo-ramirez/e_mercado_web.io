e60f60af00ef2c210e5f5d761c172a6ce3471fb7
pauliprates <pauliprat3@gmail.com>
2024-08-30 11:31:51 -0300

    funcionalidad del boton cerrar sesion y mostrar el usuario en el navbar

--- js/index.js
+++ js/index.js
@@ -22,10 +22,24 @@
 document.addEventListener("DOMContentLoaded", function(){
     // Verificar si el usuario está logueado antes de cargar cualquier otra cosa
     const logueado = localStorage.getItem('logueado');
+    const usuario = localStorage.getItem('usuarioLogeado');
+    const aMostrarUsuario = document.getElementById("liMostrarUsuario");
+    const Logout = document.getElementById("aLogout")
     if (logueado !== 'true'){
         window.location.href = '/login.html';
         return; // Detiene la ejecución del resto del código si el usuario no está logueado
     }
+    else {
+        aMostrarUsuario.innerHTML = `<span class="nav-link" id="spnUsuario">${usuario}</span>`;
+    }
+    Logout.addEventListener("click", function(){
+        localStorage.removeItem('logueado');
+        localStorage.removeItem('usuarioLogeado', null);
+
+    })
+
+    
+    
 
     // Lógica existente para manejar los clicks en las categorías
     document.getElementById("autos").addEventListener("click", function() {
