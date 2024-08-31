7aa93504123040d6a06ceef26f52744182a3b8ab
pauliprates <pauliprat3@gmail.com>
2024-08-31 17:07:38 -0300

    arreglos en el container desplazado

--- js/index.js
+++ js/index.js
@@ -23,14 +23,14 @@ document.addEventListener("DOMContentLoaded", function(){
     // Verificar si el usuario está logueado antes de cargar cualquier otra cosa
     const logueado = localStorage.getItem('logueado');
     const usuario = localStorage.getItem('usuarioLogeado');
-    const aMostrarUsuario = document.getElementById("liMostrarUsuario");
+    const liMostrarUsuario = document.getElementById("liMostrarUsuario");
     const Logout = document.getElementById("aLogout")
     if (logueado !== 'true'){
         window.location.href = 'login.html';
         return; // Detiene la ejecución del resto del código si el usuario no está logueado
     }
     else {
-        aMostrarUsuario.innerHTML = `<span class="nav-link" id="spnUsuario">${usuario}</span>`;
+        liMostrarUsuario.innerHTML = `<span class="nav-link" id="spnUsuario">${usuario}</span>`;
     }
     Logout.addEventListener("click", function(){
         localStorage.removeItem('logueado');
