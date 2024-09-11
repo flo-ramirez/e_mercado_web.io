1b0d4244b702cdd62cc4ed62a425fc416c917f84
Florencia Ramirez <floramirez1997@proton.me>
2024-09-11 09:51:19 -0300

    prueba de js - cambios en funciones

--- js/index.js
+++ js/index.js
@@ -1,57 +1,15 @@
-/*document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
-    window.onload = function(){
-        const logueado = localStorage.getItem('logueado');
-        if (logueado !== 'true'){
-            window.location = '/login.html';
-        }
-   }
-});*/
-
-document.addEventListener("DOMContentLoaded", function(){
-    // Verificar si el usuario está logueado antes de cargar cualquier otra cosa
-    const logueado = localStorage.getItem('logueado');
-    const usuario = localStorage.getItem('usuarioLogeado');
-    const liMostrarUsuario = document.getElementById("liMostrarUsuario");
-    const Logout = document.getElementById("aLogout")
-    if (logueado !== 'true'){
-        window.location.href = 'login.html';
-        return; // Detiene la ejecución del resto del código si el usuario no está logueado
-    }
-    else {
-        liMostrarUsuario.innerHTML = `<span class="nav-link" id="spnUsuario">${usuario}</span>`;
-    }
-    Logout.addEventListener("click", function(){
-        localStorage.removeItem('logueado');
-        localStorage.removeItem('usuarioLogeado', null);
-
-    })
-
-    
-    // Lógica existente para manejar los clicks en las categorías
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html";
-    });
-
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html";
-    });
-
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html";
-    });
+// Lógica existente para manejar los clicks en las categorías
+document.getElementById("autos").addEventListener("click", function() {
+    localStorage.setItem("catID", 101);
+    window.location = "products.html";
 });
+
+    document.getElementById("juguetes").addEventListener("click", function() {
+    localStorage.setItem("catID", 102);
+    window.location = "products.html";
+    });
+document.getElementById("muebles").addEventListener("click", function() {
+    localStorage.setItem("catID", 103);
+    window.location = "products.html";
+});
+
